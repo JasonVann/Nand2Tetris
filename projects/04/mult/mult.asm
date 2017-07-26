49d0e5b8707f67f55ac97753b1d4701de7207146
JasonVann <jasonvanet@gmail.com>
2017-07-26 10:19:34 +0800

    Added assmebly code for multiplication

--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -7,3 +7,40 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+// Assumes R0 >=0, R1>=0, and R0*R1 < 32768
+
+//  RAM[2] = RAM[0] * RAM[1]
+//  R2 = 0;
+//  temp = R1;
+//  while temp > 0
+//    R2 += R0;
+//    temp -=1;
+
+  @R2
+  M = 0;
+
+  @R1
+  D = M;
+  @temp
+  M = D;
+
+  (LOOP)
+    @temp
+    D = M
+    @END
+    D; JEQ
+
+    @R0
+    D = M
+    @R2
+    M = D + M // R2 += R0
+
+    @temp
+    M = M - 1
+
+    @LOOP
+    0; JMP
+
+  (END)
+  @END
+  0; JMP
